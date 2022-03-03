2841996d9ae3c087d1e023f86af5f066cf96ccd3
Rob Shelnutt <rob@blackairplane.com>
2022-03-03 17:35:38 -0500

    Remove trailing slash from hyperlink

--- src/components/styleguide/Sidebar.jsx
+++ src/components/styleguide/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = ({version}) => {
             </li>
           ))}
           <li className={"sg-sidenav--item"}>
-            <a href={`${window.location.href}${ROOT_PATH}/sb`} className="sg-sidenav--btn">View Docs</a>
+            <a href={`${window.location.href.replace(/\/+$/, '')}${ROOT_PATH}/sb`} className="sg-sidenav--btn">View Docs</a>
           </li>
         </ul>
       </nav>
